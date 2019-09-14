764398f38c150ee24229f378a5f028bb11fcf37b
Deepa <deepashri.cg@gmail.com>
2019-09-13 23:26:49 -0400

    Add a class with main method

---
+++ course-api/src/main/java/io/jb/springbootquickstart/CourseApiApp.java
@@ -0,0 +1,12 @@
+package io.jb.springbootquickstart;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseApiApp {
+
+	public static void main(String[] args) {
+
+	}
+
+}
