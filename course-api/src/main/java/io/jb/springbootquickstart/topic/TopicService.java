4d1e065dd70989a8becf262ffc3c76cf95bc85c0
Deepa <deepashri.cg@gmail.com>
2019-09-14 13:24:14 -0400

    Formatted code

--- course-api/src/main/java/io/jb/springbootquickstart/topic/TopicService.java
+++ course-api/src/main/java/io/jb/springbootquickstart/topic/TopicService.java
@@ -8,12 +8,11 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class TopicService {
-	private List<Topic> topics = new ArrayList<>(Arrays.asList(
-			new Topic("spring", "Spring Framework", "Spring Framework Description"),
-			new Topic("java", "Core Java", "Core Java Description"),
-			new Topic("javascript", "JavaScript", "Javascript Description")
-			));
-	
+	private List<Topic> topics = new ArrayList<>(
+			Arrays.asList(new Topic("spring", "Spring Framework", "Spring Framework Description"),
+					new Topic("java", "Core Java", "Core Java Description"),
+					new Topic("javascript", "JavaScript", "Javascript Description")));
+
 	public List<Topic> getAllTopics() {
 		return topics;
 	}
@@ -27,18 +26,17 @@ public class TopicService {
 	}
 
 	public void updateTopic(String id, Topic topic) {
-		for(int i = 0; i < topics.size(); i++) {
+		for (int i = 0; i < topics.size(); i++) {
 			Topic t = topics.get(i);
 			if (t.getId().equals(id)) {
 				topics.set(i, topic);
 				return;
 			}
 		}
-		
+
 	}
 
 	public void deleteTopic(String id) {
-		// TODO Auto-generated method stub
 		topics.removeIf(t -> t.getId().equals(id));
 	}
 }
