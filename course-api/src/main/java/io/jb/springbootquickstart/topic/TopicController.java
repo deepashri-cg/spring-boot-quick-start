3a9d8e87969adc0d6f765f624c2eceb9c91921d9
Deepa <deepashri.cg@gmail.com>
2019-09-14 08:30:50 -0400

    Add /topics to the controller

--- course-api/src/main/java/io/jb/springbootquickstart/topic/TopicController.java
+++ course-api/src/main/java/io/jb/springbootquickstart/topic/TopicController.java
@@ -1,5 +1,8 @@
 package io.jb.springbootquickstart.topic;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class TopicController {
 	
 	@RequestMapping("/topics")
-	public String getAllTopics() {
-		return "All Topics";
+	public List<Topic> getAllTopics() {
+		return Arrays.asList(
+			new Topic("spring", "Spring Framework", "Spring Framework Description"),
+			new Topic("java", "Core Java", "Core Java Description"),
+			new Topic("javascript", "JavaScript", "Javascript Description")
+		);
 	}
 }
