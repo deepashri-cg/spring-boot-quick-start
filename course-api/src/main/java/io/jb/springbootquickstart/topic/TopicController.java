dc80d7cf59fb72a7961480b2e85b33fc69cefe71
Deepa <deepashri.cg@gmail.com>
2019-09-14 09:03:07 -0400

    Add a Topic Service and move the list there

--- course-api/src/main/java/io/jb/springbootquickstart/topic/TopicController.java
+++ course-api/src/main/java/io/jb/springbootquickstart/topic/TopicController.java
@@ -1,20 +1,23 @@
 package io.jb.springbootquickstart.topic;
 
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TopicController {
 	
+	@Autowired
+	private TopicService topicService;
+	
+	public TopicService getTopicService() {
+		return topicService;
+	}
+
 	@RequestMapping("/topics")
 	public List<Topic> getAllTopics() {
-		return Arrays.asList(
-			new Topic("spring", "Spring Framework", "Spring Framework Description"),
-			new Topic("java", "Core Java", "Core Java Description"),
-			new Topic("javascript", "JavaScript", "Javascript Description")
-		);
+		return topicService.getAllTopics();
 	}
 }
---
+++ course-api/src/main/java/io/jb/springbootquickstart/topic/TopicService.java
@@ -0,0 +1,19 @@
+package io.jb.springbootquickstart.topic;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TopicService {
+	private List<Topic> topics = Arrays.asList(
+			new Topic("spring", "Spring Framework", "Spring Framework Description"),
+			new Topic("java", "Core Java", "Core Java Description"),
+			new Topic("javascript", "JavaScript", "Javascript Description")
+			);
+	
+	public List<Topic> getAllTopics() {
+		return topics;
+	}
+}
